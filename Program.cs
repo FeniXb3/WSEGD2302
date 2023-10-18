e3ffb01163df1ac9b78722b0930fc4251f3f7103
Konrad Gadzina <konrad@enklawa.blog>
2023-10-18 15:34:54 +0200

    Winning conditions

--- Program.cs
+++ Program.cs
@@ -11,6 +11,16 @@ if (firstSign == secondSign)
 {
     Console.WriteLine("It's a draw");
 }
+else if ((firstSign == "rock" && secondSign == "scissors") ||
+    (firstSign == "paper" && secondSign == "rock") ||
+    (firstSign == "scissors" && secondSign == "paper"))
+{
+    Console.WriteLine("player 1 won");
+}
+else
+{
+    Console.WriteLine("player 2 won");
+}
 // 1. pobierz znaki od graczy
 // 2. sprawdz czy jest remis
 // 3. sprawdz czy wygrywa gracz 1
