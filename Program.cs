7d4fcacc73089b33df569ee547bf80d711213c05
Konrad Gadzina <konrad@makegames.today>
2023-12-20 12:38:00 +0100

    Remove warnings and display points

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@ int secondPlayerPoints = 0;
 while(true)
 {
 	Console.WriteLine("Provide sign player 1");
-	string? firstSign = Console.ReadLine().ToLower().Trim();
+	string? firstSign = Console.ReadLine()?.ToLower().Trim();
 
 	while (!availableSigns.Contains(firstSign) && firstSign != "quit")
 	{
 		Console.WriteLine("wrong sign");
-		firstSign = Console.ReadLine().ToLower().Trim();
+		firstSign = Console.ReadLine()?.ToLower().Trim();
 	}
 
 	if (firstSign == "quit")
@@ -19,12 +19,12 @@ while(true)
 	}
 
 	Console.WriteLine("Provide sign player 2");
-	string? secondSign = Console.ReadLine().ToLower().Trim();
+	string? secondSign = Console.ReadLine()?.ToLower().Trim();
 
 	while (!availableSigns.Contains(secondSign) && secondSign != "quit")
 	{
 		Console.WriteLine("wrong sign");
-		secondSign = Console.ReadLine().ToLower().Trim();
+		secondSign = Console.ReadLine()?.ToLower().Trim();
 	}
 
 	if (secondSign == "quit")
@@ -35,20 +35,14 @@ while(true)
 	int secondSignIndex = Array.IndexOf(availableSigns, secondSign);
 	int spswi = (secondSignIndex + 1) % availableSigns.Length;
 	string secondSignWinningSign = availableSigns[spswi];
-	//string secondSignWinningSign = availableSigns[(Array.IndexOf(availableSigns, secondSign) + 1) % availableSigns.Length];
-
+	
 	if (firstSign == secondSign)
 	{
 		Console.WriteLine("It's a draw");
 	}
-	// else if ((firstSign == "rock" && secondSign == "scissors") ||
-	// 	(firstSign == "paper" && secondSign == "rock") ||
-	// 	(firstSign == "scissors" && secondSign == "paper"))
 	else if (firstSign == secondSignWinningSign)
 	{
-		// firstPlayerPoints = firstPlayerPoints + 1;
 		firstPlayerPoints += 1;
-		// firstPlayerPoints++;
 		Console.WriteLine("player 1 won");
 	}
 	else
@@ -56,6 +50,9 @@ while(true)
 		secondPlayerPoints += 1;
 		Console.WriteLine("player 2 won");
 	}
+
+	//Console.WriteLine("[Player 1] " + firstPlayerPoints + " : " + secondPlayerPoints + " [Player 2]");
+	Console.WriteLine($"[Player 1] {firstPlayerPoints} : {secondPlayerPoints} [Player 2]");
 }
 
 Console.WriteLine("kthxbye");
